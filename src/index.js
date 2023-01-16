28f7e4968489221827572cc1b17b572f24b2b363
SvitlanaHonchar <snidlty@gmail.com>
2023-01-16 23:38:11 +0200

    hw0.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const country = 'peru';
+
+fetchCountries(country)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
