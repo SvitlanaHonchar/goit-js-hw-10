e0eb45321fda3e9826abcc72929ebfb705980d75
SvitlanaHonchar <snidlty@gmail.com>
2023-01-17 20:26:55 +0200

    task0.2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
-const country = 'peru';
+const countryList = document.querySelector('.country-list');
+const inputEl = document.querySelector('input');
 
-fetchCountries(country)
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+const DEBOUNCE_DELAY = 300;
+// const country = 'peru';
+
+function onInput(e) {
+  const country = inputEl.value;
+  //   console.log(country);
+  fetchCountries(country)
+    .then(data => {
+      //   console.log(data);
+      const [countryData] = data;
+      countryList.innerHTML = createCountryCard(countryData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+// подія
+
+inputEl.addEventListener('input', onInput);
+
+//   карточка країни
+
+function createCountryCard(countryData) {
+  const languages = Object.values(countryData.languages).join(', ');
+
+  return `<li class="country">
+        <div class="country__header">
+          <img
+            class="country__flag"
+            src="${countryData.flags.svg}"
+            alt="country flag"
+            height="35"
+          />
+          <p class="country__name">${countryData.name.official}</p>
+        </div>
+        <p><span>Capital:</span> ${countryData.capital}</p>
+        <p><span>Population:</span> ${countryData.population}</p>
+        <p><span>Languages:</span> ${languages}</p>
+      </li>`;
+}
