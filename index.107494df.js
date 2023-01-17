1e6c493529755dfcf93da023879edd8aac6aedd7
SvitlanaHonchar <113723756+SvitlanaHonchar@users.noreply.github.com>
2023-01-17 18:27:24 +0000

    Deploying to gh-pages from @ SvitlanaHonchar/goit-js-hw-10@e0eb45321fda3e9826abcc72929ebfb705980d75 🚀

--- index.107494df.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(n="peru",fetch("".concat("https://restcountries.com","/v3.1/name/").concat(n,"?fields=name,capital,population,flags,languages")).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()}))).then((function(n){console.log(n)})).catch((function(n){console.log(n)}))}();
-//# sourceMappingURL=index.107494df.js.map
--- index.5dc3bc10.js
+++
@@ -1,2 +0,0 @@
-var e;(e="peru",fetch(`https://restcountries.com/v3.1/name/${e}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()}))).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.5dc3bc10.js.map
